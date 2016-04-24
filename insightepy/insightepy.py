f47014124ab492830b06f183b1372bc3d165d149
arj-196 <arjruler93@gmail.com>
2016-04-24 12:22:38 +0200

    initial

---
+++ insightepy/insightepy.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import sys, traceback
+from urllib3 import HTTPConnectionPool
+import json
+import resources as rs
+
+
+class API(object):
+    def __init__(self, clientid, clientsecret, authtoken):
+        self._cid = clientid
+        self._csecret = clientsecret
+        self._authtoken = authtoken
+        self.pool = HTTPConnectionPool(rs.HOST_ADDR + ':' + rs.HOST_PORT)
+
+    def make_request(self, method, url, fields):
+        r = self.pool.request(method, url, fields=fields)
+        try:
+            return json.loads(r.data)
+        except:
+            return r.data
+
+    def single_extract(self,
+                       verbatim, lang,
+                       ifterm=True, ifkeyword=True, ifconcept=True,
+                       ifpos=True, ifemotion=True, ifsentiment=True, ifNER=True,
+                       ifHashTags=True, ifMentions=True, ifUrl=True
+                       ):
+
+        # make request to REST_API
+        r = self.make_request('GET', '/extract', {
+            'verbatim': verbatim,
+            'lang': lang,
+            'ifterm': ifterm,
+            'ifkeyword': ifkeyword,
+            'ifconcept': ifconcept,
+            'ifpos': ifpos,
+            'ifemotion': ifemotion,
+            'ifsentiment': ifsentiment,
+            'ifner': ifNER,
+            'ifhashtags': ifHashTags,
+            'ifmention': ifMentions,
+            'ifurl': ifUrl,
+        })
+        return r
+
+    def batch_extract(self,
+                      filepath,
+                      ifterm=True, ifkeyword=True, ifconcept=False,
+                      ifpos=True, ifemotion=True, ifsentiment=True, ifNER=True,
+                      ifHashTags=True, ifMentions=True, ifUrl=True
+                      ):
+        pass
